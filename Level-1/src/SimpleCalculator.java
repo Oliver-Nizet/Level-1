008000f8d9105292a9558fa54cf21822f11ea929
Stephen Cerruti <scerruti@otabi.com>
2017-02-14 18:21:34 -0800

    hello

--- Level-1/src/SimpleCalculator.java
+++ Level-1/src/SimpleCalculator.java
@@ -1,14 +1,15 @@
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
-public class SimpleCalculator {
+import java.io.IOException;
+public class SimpleCalculator implements ActionListener{
 	JFrame jframe;
 	JButton add;
 	JButton sub;
@@ -47,18 +48,69 @@ public class SimpleCalculator {
 		jframe.add(numbers);
 		jframe.add(buttons);
 		jframe.add(answer);
+		add.addActionListener(this);
+		sub.addActionListener(this);
+		mul.addActionListener(this);
+		div.addActionListener(this);
 		jframe.setTitle("Simple Calculator");
 		add.setText("+");
 		sub.setText("-");
 		mul.setText("x");
 		div.setText("÷");
-		num1.setText("        ");
-		num2.setText("        ");
-		output.setText("                          ");
+		num1.setText("Number 1");
+		num2.setText("Number 2");
+		output.setText("Put in Two Numbers and Press an Operator");
 		jframe.pack();
+		speak("Put in Two Numbers and Press an Operator");
 	}
 	public static void main(String[] args) {
 		SimpleCalculator sc = new SimpleCalculator();
 		Scanner s = new Scanner(System.in);
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			
+		
+		String leftText = num1.getText();
+		String rightText = num2.getText();
+		float leftyText = Float.parseFloat(leftText);
+		float rightyText = Float.parseFloat(rightText);
+		if (e.getSource()==add) {
+			float addAnswer = leftyText + rightyText;
+			String addyAnswer = Float.toString(addAnswer);
+			output.setText("Answer: " + addyAnswer);
+			speak("The answer is " + addyAnswer);
+		}
+		if (e.getSource()==sub) {
+			float subAnswer = leftyText - rightyText;
+			String subyAnswer = Float.toString(subAnswer);
+			output.setText("Answer: " + subyAnswer);
+			speak("The answer is " + subyAnswer);
+		}
+		if (e.getSource()==mul) {
+			float mulAnswer = leftyText * rightyText;
+			String mulyAnswer = Float.toString(mulAnswer);
+			output.setText("Answer: " + mulyAnswer);
+			speak("The answer is " + mulyAnswer);
+		}
+		if (e.getSource()==div) {
+			float divAnswer = leftyText / rightyText;
+			String divyAnswer = Float.toString(divAnswer);
+			output.setText("Answer: " + divyAnswer);
+			speak("The answer is " + divyAnswer);
+		}
+		} catch (Exception e2) {
+			output.setText("Put in Valid Numbers");
+			speak("Put in Valid Numbers");
+		}
+	}
+	static void speak(String words) {
+	   	try {
+	   		Runtime.getRuntime().exec("say " + words).waitFor();
+	   	} catch (Exception e) {
+	   		e.printStackTrace();
+	   	}
+	}
 }
+
