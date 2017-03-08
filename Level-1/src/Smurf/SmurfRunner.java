aece99e8641c091b4d562cf37167eaaa3dc1d2ee
Oliver-Nizet <league@WorthyMini.attlocal.net>
2017-03-07 18:23:28 -0800

    helo

---
+++ Level-1/src/Smurf/SmurfRunner.java
@@ -0,0 +1,17 @@
+package Smurf;
+
+public class SmurfRunner {
+	public static void main(String[] args) {
+		Smurf Handy = new Smurf("Handy");
+		System.out.println(Handy.getName());
+		Handy.eat();
+		Smurf Papa = new Smurf("Papa");
+		System.out.println(Papa.getName());
+		System.out.println(Papa.getHatColor());
+		System.out.println(Papa.isGirlOrBoy());
+		Smurf Smurfette = new Smurf("Smurfette");
+		System.out.println(Smurfette.getName());
+		System.out.println(Smurfette.getHatColor());
+		System.out.println(Smurfette.isGirlOrBoy());
+	}
+}
